f95fadd9846228951a01ac88b4167b9c03f74890
Don Wood <donwood@gmail.com>
2020-11-08 22:06:00 -0600

    Fixed Harris Detector and added began working on Harris Matching.

--- ch02/harris_detector.py
+++ ch02/harris_detector.py
@@ -3,9 +3,11 @@ import numpy as np
 import pylab as plt
 from common import harris
 
+threshold = 0.1
+
 im = np.array(Image.open('images/empire.jpg').convert('L'))
 harrisim = harris.compute_harris_response(im)
-filtered_coords = harris.get_harris_points(harrisim,6)
+filtered_coords = harris.get_harris_points(harrisim,6,threshold)
 harris.plot_harris_points(im, filtered_coords)
 
 # plt.figure("Harris Corner Detector")
